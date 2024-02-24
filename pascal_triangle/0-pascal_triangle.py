5e1d53f99e1bc3818cfc44ad990f06ce44c85830
Eliane_M <munezeroeliane761@gmail.com>
2024-02-24 12:40:43 -0800

    pascals triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/binpython3
+"""
+Returns a list of integers
+representing the pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    pascals triangle calculations
+    """
+    if n<= 0:
+        return []
+
+    triangle = []
+    row = []
+    prev_row = []
+    for m in range(0, n + 1):
+        row = [k > 0 and k < m - 1 and m > 2 and prev_row[k-1] +
+               prev_row[k] or 1 for k in range(0, m)]
+        prev_row = row
+        triangle += [row]
+    return triangle[1:]
---
+++ pascal_triangle/README.md
@@ -0,0 +1,1 @@
+Calculations for pascal's triangle
